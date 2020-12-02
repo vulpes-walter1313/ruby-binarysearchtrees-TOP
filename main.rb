28f00d1d784d2658da3c13df2d9b6481ebdf52b0
Walter Fritsche <wfritsche95@protonmail.com>
2020-12-02 15:25:29 -0600

    main tests

--- main.rb
+++ main.rb
@@ -8,4 +8,11 @@ end
 
 test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 # test = Tree.new([1, 2, 3, 4, 5, 6, 7])
+# test = Tree.new([10, 20, 30, 100, 500])
+pretty_print(test.root)
+# p test.find(test.root, 23)
+puts "\nInserting: 44"
+test.insert(test.root, 68)
+test.insert(test.root, 99)
+test.insert(test.root, 45)
 pretty_print(test.root)
